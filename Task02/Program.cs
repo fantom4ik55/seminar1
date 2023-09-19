0ae02190d3065d3294ca54c0f42b83337a16100c
Alex84 <fantom.rpg@mail.ru>
2023-09-19 10:02:14 +0300

    Домашняя работа

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -11,9 +11,9 @@
 Console.WriteLine("Пожалуйста введите 2 числа,");
 Console.WriteLine("чтобы узнать какое большее, а какое меньшее.");
 Console.Write("Введите первое число: ");
-int number1 = int.Parse(Console.ReadLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int number2 = int.Parse(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
     Console.WriteLine("Число " + number1 + " большее");
