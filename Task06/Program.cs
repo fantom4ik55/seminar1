721eb8fc4b678e2592634395e983cc334327d90f
Alex84 <fantom.rpg@mail.ru>
2023-09-19 15:50:57 +0300

    Исправил на просто Четное Нечетное

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -13,8 +13,8 @@
 Console.WriteLine("Введите число что узнать четное оно или нет: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a % 2 == 0)
-    Console.WriteLine("Число: " + a + "четное");
+    Console.WriteLine("Четное");
 else
 {
-    Console.WriteLine($"Число: {a} нечетное");
+    Console.WriteLine($" Нечетное");
 }
