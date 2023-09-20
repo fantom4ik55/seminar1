30f379d11046a29d83709f78eed05f335f504123
Alex84 <fantom.rpg@mail.ru>
2023-09-20 09:54:16 +0300

    Исправил значения переменной с# на маленькую букву

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -10,9 +10,9 @@
 
 
 Console.WriteLine("Введите число");
-int Number = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-for (int index = 1; index <= Number; index++)
+for (int index = 1; index <= number; index++)
 {
     if (index % 2 == 0)
         Console.WriteLine(index + ",");
