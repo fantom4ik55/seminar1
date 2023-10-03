1da7969decf207f08ae6779e2df2c4bcb8b5244a
Alex84 <fantom.rpg@mail.ru>
2023-10-03 17:38:33 +0300

    ДЗ

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -20,7 +20,7 @@ void FillArrayRandomNumbers(int[] numbers)
 {
     for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(1, 100);
+        numbers[i] = new Random().Next(-100, 100);
     }
 }
 
